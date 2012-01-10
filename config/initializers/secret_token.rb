12301d4c135e9ec945914b41cc0caec2ce37d943
Denis V. Bakaev <Kiragods@gmail.com>
2012-01-10 23:52:44 +0200

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rezjume::Application.config.secret_token = '1a19fe0acdc508bfe7661e403fe0eaa045cc5164bc1e5509cd20c1da6444aff14fadd205fa52ce2b6f44ce070c63e182ddba4436878330762d0d82284b3e4f85'
